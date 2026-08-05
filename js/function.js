a4d395f7a26591c126b5463a09b0e920ee91d3ce
mafdeashak-dot <mafdeashak@gmail.com>
2026-08-05 17:37:34 +0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -13,3 +13,27 @@ scrollUp.addEventListener("click", function () {
     behavior: "smooth"
   });
 });
+
+
+const searchInput = document.getElementById("searchInput");
+const exerciseCards = document.querySelectorAll(".exercise-card");
+const noResults = document.getElementById("noResults");
+
+searchInput.addEventListener("input", function () {
+
+    const searchValue = this.value.trim().toLowerCase();
+    let found = false;
+
+    exerciseCards.forEach(card => {
+        const text = card.textContent.toLowerCase();
+
+        if (text.includes(searchValue)) {
+            card.style.display = "flex";
+            found = true;
+        } else {
+            card.style.display = "none";
+        }
+    });
+
+    noResults.style.display = found ? "none" : "block";
+});
